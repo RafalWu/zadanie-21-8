e4bb9767f47d6761e6225f24c1a5fe8a697072bd
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-04-18 13:22:25 +0200

    Modify one

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const User = mongoose.model('User', userSchema);
 
 mongoose.connect('mongodb://localhost/nodeappdatabase', {
     useMongoClient: true
@@ -10,4 +11,26 @@ const userSchema = new Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     admin: Boolean
+});
+
+userSchema.methods.manify = function(next) {
+    this.name = this.name + '-boy';
+
+    return next(null, this.name);
+};
+
+userSchema.pre('save', function(next) {
+    //pobranie aktualnego czasu
+    const currentDate = new Date();
+
+    //zmiana pola na aktualny czas
+    this.updated_at = currentDate;
+
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+
+    // next() jest funkcją która przechodzi do następnego hooka do
+    // wykonania przed lub po requeście
+    next();
 });
